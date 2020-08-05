f203bf15661410749fb310797534b06440981b32
Daniel Gould <dmgould310@gmail.com>
2020-08-05 19:13:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,11 @@ class Song
   def self.genre_count 
     genre_count = {}
       @@genres each do |genre|
-      if genre[]
+      if genre_count[genre]
+        genre_count +=1 
+      else
+        
+      end 
       end 
   end 
 

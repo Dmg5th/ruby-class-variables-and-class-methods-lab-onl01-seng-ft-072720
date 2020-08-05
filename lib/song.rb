14b48eaa590863c3a05fcec999b02222e0e31c6c
Daniel Gould <dmgould310@gmail.com>
2020-08-05 17:20:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,16 @@ class Song
 
   @@count = 0 
   
+  
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist 
     @genre = genre 
     @@count += 1 
+  end 
+  
+  def self.count
+    @@count   
   end 
 
 
